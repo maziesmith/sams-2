41c5791f5aa7a299b0fa8572b36bd3c792aa3c6c
John Lioneil Dionisio <john.dionisio1@gmail.com>
2016-06-28 18:29:35 +0800

    fixed constructor

---
+++ application/controllers/MessagesController.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MessagesController extends CI_Controller {
+
+    private $Data = array();
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->validated();
+        $this->Data['Headers'] = get_page_headers();
+    }
+
+    public function validated()
+    {
+        $this->session->set_userdata('error', "You are not logged in");
+        if(!$this->session->userdata('validated')) redirect('login');
+    }
+
+    /**
+     * Index Page for this controller.
+     *
+     * @see http://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index()
+    {
+        // $this->Data['contacts'] = $this->Contact->all();
+        // $this->Data['form']['groups_list'] = dropdown_list($this->Group->dropdown_list('groups_id, groups_name')->result_array(), ['groups_id', 'groups_name'], 'No Group');
+        // $this->Data['form']['levels_list'] = dropdown_list($this->Level->dropdown_list('levels_id, levels_name')->result_array(), ['levels_id', 'levels_name'], 'No Level');
+        // $this->Data['form']['types_list']  = dropdown_list($this->Type->dropdown_list('types_id, types_name')->result_array(), ['types_id', 'types_name'], 'No Type');
+        $this->load->view('layouts/main', $this->Data);
+    }
+
+}
